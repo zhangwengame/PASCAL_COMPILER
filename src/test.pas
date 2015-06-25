289964682d52e80732b6c6d97df551bdb97f2d15
Bran <544524199@qq.com>
2015-06-25 22:16:06 +0800

    test modify

--- src/test.pas
+++ src/test.pas
@@ -1,9 +1,9 @@
 program a; 
 var
-i,j,z:integer;
+z:integer;
 a,b,c:real;
 begin
-a:=0.2;
+a:=1.2;
 writeln(a);
 b:=-0.3;
 writeln(b);
@@ -12,35 +12,7 @@ writeln(c);
 c:=a*b;
 writeln(c);
 c:=a/b;
-writeln(c);
-if c<0 then
- writeln(123)
-else
- writeln(0);
-
-if c<=1.0 then
- writeln(321)
-else
- writeln(111);
-
-if c=-0.666667 then 
- writeln(666)
-else 
-begin
- writeln(999);
-end;
-
-i:=2;
-writeln(i);
-j:=-3;
-writeln(j);
-z:=i*j;
-writeln(z);	
-z:=i mod j;
-writeln(z);
-
-if i < j then
- writeln(123456)
-else
- writeln(654321);
+i:=1+i*2;
+j:=1;
+a:=z;
 end.
