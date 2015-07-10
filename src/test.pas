0f187b7f767bb959c1455cb8cc2bf6551d7c7290
everskar2013 <dtsps.skar@gmail.com>
2015-07-10 16:17:30 +0800

    fix invisible bug on type cast

--- src/test.pas
+++ src/test.pas
@@ -1,9 +1,49 @@
 program a; 
 var
-b,c:real;
+i,j,z:integer;
+a,b,c:real;
 ch:char;
 begin
+    a:=0.2;
+    writeln(a);
     b:=-0.3;
+    writeln(b);
+    c:=a+b-0.001;
+    writeln(c);
+    c:=a*b;
+    writeln(c);
+    c:=a/b;
+    writeln(c);
+    if c<0.1 then
+        writeln(123)
+    else
+        writeln(0);
+
+    if c<=1.0 then
+        writeln(321)
+    else
+        writeln(111);
+
+    if c=-0.666667 then 
+        writeln(666)
+    else 
+    begin
+        writeln(111);
+        writeln(999);
+    end;
+
+    i:=2;
+    writeln(i);
+    j:=-3;
+    writeln(j);
+    c:=i*j;
+    writeln(c);
+    z:=i mod j;
+    writeln(z);
+    a:=0.2;
+    b:=-0.3;
+    c:=a+b-0.001;
+    writeln(c);
 
     ch:='a';
     writeln(ch);
@@ -11,4 +51,35 @@ begin
     c:=ch*b;
     writeln(c); 
 
+    i:=21;  
+    c:=a*i;
+    writeln(c);
+
+    c:=a/b;
+    writeln(c);
+
+
+    if c<1 then
+        writeln(123)
+    else
+        writeln(0);
+    if c<=1.0 then
+        writeln(321)
+    else
+        writeln(111);
+    if c=-0.666667 then 
+         writeln(666)
+    else 
+    begin
+        writeln(111);
+        writeln(999);
+    end;
+    i:=2;
+    writeln(i);
+    j:=-3;
+    writeln(j);
+    z:=i*j;
+    writeln(z);
+    z:=i mod j;
+    writeln(z);
 end.
